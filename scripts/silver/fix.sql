5e5c61376d6a265d952ee064751b777727ac953b
shwethabejjanki <shwethabejjanki@gmail.com>
2026-02-06 12:17:55 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -133,3 +133,19 @@ case
  else 'n/a'
 end as gen
 FROM bronze.erp_cust_az12
+
+
+
+insert into silver.erp_loc_a101(
+cid,
+cntry
+)
+SELECT 
+REPLACE(cid,'-','') as cid,
+case 
+ when trim(cntry) = 'DE' then 'Germany'
+ when trim(cntry) in ('US','USA') then 'United States'
+ when trim(cntry) = '' or cntry is null then 'n/a'
+ else trim(cntry)
+end as cntry
+FROM bronze.erp_loc_a101;
