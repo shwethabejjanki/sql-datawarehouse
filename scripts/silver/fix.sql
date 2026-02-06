9d7fa28eefc2ee661b66c55a4081b42c54b09bda
shwethabejjanki <shwethabejjanki@gmail.com>
2026-02-06 11:53:31 +0530

    Update fix.sql

--- scripts/silver/fix.sql
+++ scripts/silver/fix.sql
@@ -112,3 +112,24 @@ case
   else sls_price
 end as sls_price
 from bronze.crm_sales_details 
+
+
+insert into silver.erp_cust_az12(
+cid,
+bdate,
+gen)
+SELECT
+case 
+ when cid like 'NAS%' then SUBSTRING(cid,4,len(cid)) 
+ else cid
+ end as cid_,
+case
+ when bdate > getdate() then NULL
+ else bdate
+ end as bdate,
+case 
+ when UPPER(TRIM(GEN)) in ('M','MALE') then 'Male'
+ when upper(trim(GEN)) in ('F','FEMALE') then 'Female'
+ else 'n/a'
+end as gen
+FROM bronze.erp_cust_az12
